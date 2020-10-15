edc540baf8d283e72e5d227c4483dc34cb53e305
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-10-15 16:38:34 -0500

    edit readbook & readingbook & connected to service

--- ClientApp/src/app/service/book-service.service.ts
+++ ClientApp/src/app/service/book-service.service.ts
@@ -2,11 +2,16 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Ibook } from '../interfaces/ibook';
 import { BookComponent } from '../book/book.component';
+import { ReadBookComponent } from '../read-book/read-book.component';
+import { ReadingBookComponent } from '../reading-book/reading-book.component';
 
 @Injectable({
     providedIn: 'root'
   })
   export class BookService {
+
+    books: Ibook[];
+    statuses = ['Want to Read', 'Reading', 'Read'];
 
     constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
