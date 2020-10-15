ead47707b2bc0fd5238029f0dba24f4ea5be6e93
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-10-15 12:25:30 -0500

    created book interface

---
+++ ClientApp/src/app/interfaces/ibook.ts
@@ -0,0 +1,10 @@
+export interface Ibook {
+    id: string;
+    title: string;
+    author: string;
+    genre: string;
+    chapters: number;
+    yearOfPublish: Date;
+    description: string;
+    status: string;
+}
