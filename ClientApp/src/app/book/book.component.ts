a45d7e634a9c9ed0e0f8929f59e177c72150b26a
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-10-15 12:55:21 -0500

    added routes to page

---
+++ ClientApp/src/app/book/book.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Ibook } from '../interfaces/ibook';
+import { BookService } from '../service/book-service.service';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+
+  public bookList: Ibook[];
+  public newBook: Ibook = {
+    id: undefined,
+    title: '',
+    author: '',
+    genre: '',
+    chapters: 0,
+    yearOfPublish: null,
+    description: '',
+    status: '',
+  };
+
+  constructor(private bookService: BookService) { }
+
+  async ngOnInit() {
+    this.bookList = await this.bookService.getBook();
+  }
+
+  public async addBook() {
+    const newBook = await this.bookService.addBook(this.newBook);
+    this.bookList.push(newBook);
+  }
+}
---
+++ ClientApp/src/app/service/book-service.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Ibook } from '../interfaces/ibook';
+import { BookComponent } from '../book/book.component';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class BookService {
+
+    constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+
+    public async getBook(): Promise<Ibook[]> {
+      return this.httpClient.get<Ibook[]>(`${this.baseUrl}book`).toPromise();
+    }
+
+    public async addBook(book: Ibook): Promise<Ibook> {
+      return this.httpClient.post<Ibook>(`${this.baseUrl}book`, book).toPromise();
+    }
+}
