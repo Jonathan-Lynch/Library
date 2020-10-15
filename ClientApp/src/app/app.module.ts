43bb9af73a986bfdba648a120d36bb9041330b58
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-10-15 13:05:01 -0500

    removed links to other apps. purge part 1

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,13 +6,9 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { StudentsComponent } from './students/students.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
-import { TeacherComponent } from './teacher/teacher.component';
 import { BookComponent } from './book/book.component';
 import { LoginComponent } from '../api-authorization/login/login.component';
 
@@ -21,10 +17,6 @@ import { LoginComponent } from '../api-authorization/login/login.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    StudentsComponent,
-    TeacherComponent,
     BookComponent,
   ],
   imports: [
@@ -34,10 +26,6 @@ import { LoginComponent } from '../api-authorization/login/login.component';
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-        { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-        { path: 'students', component: StudentsComponent, canActivate: [AuthorizeGuard] },
-        { path: 'teachers', component: TeacherComponent, canActivate: [AuthorizeGuard] },
         { path: 'books', component: BookComponent, canActivate: [AuthorizeGuard] },
         { path: '**', component: LoginComponent}
     ])
